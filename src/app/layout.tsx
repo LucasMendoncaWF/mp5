7dc9da8826edda0a345ba5e30d2935f22ea77e03
unknown <lucasmendoncawf@gmail.com>
2025-05-07 16:47:59 -0300

    fix intl to work with netlifh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,17 @@
-import { headers } from 'next/headers';
-import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import React from 'react';
 
 import Header from '@/components/PageStructure/Header';
 import PreRender from '@/components/Shared/PreRender';
-import { routing } from '@/i18n/routing';
-import nextIntlConfig from '@/locales/languages';
-
 import './globals.scss';
+import { getLanguageFromHeaders, getThemeFromHeaders } from '@/utils/handleHeaders';
 
-async function getThemeFromHeaders() {
-  const headersList = headers();
-  const theme = (await headersList).get('theme');
-  return theme === 'light' ? 'light' : 'dark';
-}
-
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
-}) {
+export default async function LocaleLayout({ children }: { children: React.ReactNode }) {
   const theme = await getThemeFromHeaders();
-  const { locale } = await params;
-  let newLocale = await nextIntlConfig.defaultLocale;
-  if (hasLocale(routing.locales, locale)) {
-    newLocale = locale;
-  }
+  const locale = await getLanguageFromHeaders();
 
   return (
-    <html lang={newLocale} data-theme={theme} className={theme}>
+    <html lang={locale} data-theme={theme} className={theme}>
       <head>
         <title>Music Player</title>
         <meta name="description" content="Music Player demo for portfolio" />
