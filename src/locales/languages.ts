60bdd211f1dcb5a4ed0a5e096c2d7a1644678251
unknown <lucasmendoncawf@gmail.com>
2025-05-08 12:11:30 -0300

    fixes

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -1,4 +1,10 @@
-const nextIntlConfig = {
+import type { LocalePrefix, LocalePrefixMode } from 'next-intl/routing';
+
+const nextIntlConfig: {
+  locales: string[];
+  defaultLocale: string;
+  localePrefix: LocalePrefix<string[], LocalePrefixMode>;
+} = {
   locales: ['en', 'pt'],
   defaultLocale: 'en',
   localePrefix: 'never',
